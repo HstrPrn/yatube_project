6b755dd7baedd11254042ae74bb7ed145359262f
Ivan Efremov <socrew112@gmail.com>
2023-02-05 18:19:56 +0300

    Update posts.urls yatube.urls posts.views

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,1 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Главная страниц')
+
+
+def group_posts(request):
+    return HttpResponse('Группы')
+
+
+def group_details(request, slug):
+    return HttpResponse(f'Посты группы {slug}')
+
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('group/<slug:slug>/', include('posts.urls')),
+    path('group/', include('posts.urls')),
     path('', include('posts.urls')),
     path('admin/', admin.site.urls),
 ]
